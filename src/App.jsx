c897b10054d542d70791bc73d9463861edd69745
Rana Sarkar <rana.sarkarg3@gmail.com>
2024-02-26 22:38:21 +0530

    firt commit for frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center font-sans">
+        <h1>Official Web Page of NSP</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
