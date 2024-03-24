e7ec2cce17e142a3ac0906e53bbd898c9e957adc
Subhrajitff <subhrajitsaha115@gmail.com>
2024-03-24 10:37:29 +0530

    added Footer.jsx,Footer.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
 
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function App() {
   return (
     <>
       <div className="bg-slate-800">
         <Navbar />
       </div>
-      <div className="text-3xl text-center mx-10 font-sans font-bold">
+      <div className="body_sty text-3xl text-center mx-10 font-sans font-bold height-300px">
         <h1>Official Website of NITA South Paribar</h1>
+      
+
+     
       </div>
+    <Footer /> 
     </>
   );
 }
