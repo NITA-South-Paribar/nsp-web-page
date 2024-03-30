c940d9d8793b132d4dc91805da3227cb0cfcbc8e
Rana Sarkar <rana.sarkarg3@gmail.com>
2024-03-31 01:43:58 +0530

    Resolved the error in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Footer } from "./components/Footer/Footer";
 
 import './App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function App() {
   return (
     <>
