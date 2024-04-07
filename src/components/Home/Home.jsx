aa6eb249266daaa58f308544f883ed92145e67b9
PRITAM PAUL <paul.pritamg3@gmail.com>
2024-04-07 16:31:57 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const Home = () => {
   return (
     <div className="ml-[510px] mt-5 text-5xl px-5 py-3 font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-      Official Website of NITA South Paribar
+      <h1>Official Website of NITA South Paribar</h1>
       <div className="h-[1000px] " >
-          <a href="https://chat.whatsapp.com/IlR4GvrCtNBLpXeVHWXJpM">Join Our Comunity in WhatsApp</a>
+          <h2>Join Our Comunity in WhatsApp</h2>
+          <a href="https://chat.whatsapp.com/IlR4GvrCtNBLpXeVHWXJpM"><b>LINK</b></a>
       </div>
     </div>
   );
