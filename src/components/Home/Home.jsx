1bdd255f34c4719d81049438897a50f690aa8f32
PRITAM PAUL <paul.pritamg3@gmail.com>
2024-04-07 16:27:15 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="ml-[510px] mt-5 text-5xl px-5 py-3 font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
       Official Website of NITA South Paribar
       <div className="h-[1000px] " >
-          This is demo text
+          <a href="https://chat.whatsapp.com/IlR4GvrCtNBLpXeVHWXJpM">Join Our Comunity in WhatsApp</a>
       </div>
     </div>
   );
