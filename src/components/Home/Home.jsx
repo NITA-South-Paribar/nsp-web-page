14e92033cfb3027cc07657919945f379891dcfa9
Soumyadip Bhowmik <sdipbhk@gmail.com>
2024-04-20 01:12:54 +0530

    updated Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,43 @@
 import { FaExternalLinkAlt } from "react-icons/fa";
+import backgroundImage from '/src/assets/background.jpg';
+import {motion} from 'framer-motion'
 
 const Home = () => {
   return (
-    <div className="ml-[22%] mt-5 text-5xl px-5 py-3 font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-      <h1>Official Website of NITA South Paribar</h1>
-      <div className="h-[1000px] flex gap-5 mt-1" >
-          <h2>Join Our Comunity in WhatsApp</h2>
-          <a href="https://chat.whatsapp.com/IlR4GvrCtNBLpXeVHWXJpM" target="_blank">
-            <FaExternalLinkAlt className="text-black "/>
+    <div className="relative h-screen overflow-hidden">
+      <div className="absolute inset-0 mt-12">
+  <div 
+    className="absolute inset-0 bg-contain bg-center" 
+    style={{ 
+      backgroundImage: `url(${backgroundImage})`, 
+      backgroundRepeat: 'no-repeat', 
+      backgroundPosition: 'center', 
+      backgroundSize: 'cover', 
+      opacity: 0.40 
+    }}
+  ></div>
+  <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-[#f5f5dc]"></div>
+</div>
+
+      <motion.div initial={{y:100,opacity: 0}} animate={{y:0, opacity: 1}} transition={{
+          duration:"1",
+          delay:".5"
+      }} className="absolute inset-0 flex flex-col items-center justify-center z-1">
+        <div className="text-8xl text-center font-bold mb-4 text-slate-800 tracking-tight Heading" style={{ textShadow: '2px 2px #fff' }}>
+          WELCOME TO NSP
+        </div>
+        <div className="text-lg max-w-4xl text-center mb-6 px-20 text-slate-700">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta totam recusandae, architecto dicta fugit laborum illo eveniet. Ab dolorum eius quaerat accusantium dolores quisquam, temporibus voluptates ducimus a doloremque fugit!
+        </div>
+        <div>
+          <a href="https://chat.whatsapp.com///IlR4GvrCtNBLpXeVHWXJpM" target="_blank" rel="noopener noreferrer">
+            <button className="flex items-center text-white font-black bg-gradient-to-r from-green-500 to-green-900 py-2 px-4 rounded-md cursor-pointer hover:bg-gradient-to-t hover:from-green-400 hover:to-green-900">
+              Join our community
+              <FaExternalLinkAlt className="ml-2" />
+            </button>
           </a>
-      </div>
+        </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,34 +2,44 @@ import { Link, NavLink } from "react-router-dom";
 import logo from "../../../src/assets/NSP-Logo.png";
 import { navItems } from "../../constants";
 import { Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { SigninModal } from "../SigninModal/SigninModal";
+
 export const Navbar = () => {
   const [openModal, setOpenModal] = useState(false);
   const [mobileDraweOpen, setMobileDraweOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
   const toggleNavbar = () => {
     setMobileDraweOpen(!mobileDraweOpen);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 100);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible]);
+
   return (
-    <nav className="sticky top-0 z-50 py-3 bg-slate-800 mx-auto my-5 rounded-lg text-white w-3/4 px-4">
+    <nav className={"fixed top-0 z-50 py-3 bg-slate-800 mx-auto text-white w-full px-4" + (visible ? " " : " hidden")}>
       <div className="container mx-auto relative text-md font-semibold">
         <div className="flex justify-between items-center">
-          <Link
-            to=""
-            className="flex items-center flex-shrink-0 cursor-pointer"
-          >
+          <Link to="" className="flex items-center flex-shrink-0 cursor-pointer">
             <img className="h-10 w-12" src={logo} alt="" />
             <span className="text-2xl mx-1 tracking-tight font-sans">NSP</span>
           </Link>
           <ul className="hidden lg:flex ml-14 space-x-10">
             {navItems.map((item, index) => (
               <li key={index}>
-                <NavLink
-                  to={item.href}
-                  className="cursor-pointer rounded-lg py-2 px-2 hover:bg-gray-700"
-                >
+                <NavLink to={item.href} className="cursor-pointer rounded-lg py-2 px-2 hover:bg-gray-700">
                   {item.label}
                 </NavLink>
               </li>
@@ -37,10 +47,7 @@ export const Navbar = () => {
           </ul>
           <div className="hidden lg:flex justify-center space-x-6 items-center">
             <div>
-              <button
-                onClick={() => setOpenModal(true)}
-                className="text-yellow-500 hover:text-white rounded-lg py-2 px-4 text-center hover:bg-gray-700"
-              >
+              <button onClick={() => setOpenModal(true)} className="text-yellow-500 hover:text-white rounded-lg py-2 px-4 text-center hover:bg-gray-700">
                 Sign in
               </button>
               <SigninModal
@@ -49,7 +56,6 @@ export const Navbar = () => {
                 onClose={() => setOpenModal(false)}
               />
             </div>
-
             <Link to="signup">
               <button className="bg-gradient-to-r from-green-500 to-green-900 py-2 px-4 rounded-md cursor-pointer hover:bg-gradient-to-t hover:from-green-400 hover:to-green-900">
                 Member Sign Up
@@ -65,7 +71,7 @@ export const Navbar = () => {
 
         {/* Responsive Navbar Starts here */}
         {mobileDraweOpen && (
-          <div className="fixed right-0 z-20 bg-gray-300 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+          <div className="fixed top-16 right-0 z-50 bg-gray-300 w-full p-12 flex flex-col justify-center items-center lg:hidden">
             <ul>
               {navItems.map((item, index) => (
                 <li key={index} className="py-4 cursor-pointer">
@@ -74,16 +80,10 @@ export const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-              <Link
-                to=""
-                className="py-2 px-3 rounded-md border cursor-pointer"
-              >
+              <Link to="" className="py-2 px-3 rounded-md border cursor-pointer">
                 Sign In
               </Link>
-              <Link
-                to=""
-                className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md cursor-pointer"
-              >
+              <Link to="" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md cursor-pointer">
                 Sign Up
               </Link>
             </div>
